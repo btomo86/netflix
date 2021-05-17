c201504a6568b2d575c92eb34e24eb09e0d584e4
Ben Thompson <btomo86@gmail.com>
2021-05-17 21:27:15 +0100

    created the header for the  home page and styled it

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { FaqsContainer } from "../containers/faq";
 import { FooterContainer } from "../containers/footer";
+import HeaderContainer from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
+import { Feature, OptForm } from "../components";
 
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimted films, TV programmes and more.</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. cancel at any time
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input playholder="Email Address" />
+            <OptForm.Button>Try Now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
