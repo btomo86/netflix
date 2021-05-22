5f5bbeaf4aa167c98f964e1fc8c932e93becf4ed
Ben Thompson <btomo86@gmail.com>
2021-05-22 09:17:06 +0100

    styled the signin form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { FooterContainer } from "../containers/footer";
 import HeaderContainer from "../containers/header";
+import { Form } from "../components";
 
 export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
-      <HeaderContainer>Hello from the sign in!</HeaderContainer>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit
+              disabled={isInvalid}
+              type="submit"
+              data-testid="sign-in"
+            >
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google ReCAPTCHA to ensure you're not a
+            bot. learn more
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
       <FooterContainer />
     </>
   );
