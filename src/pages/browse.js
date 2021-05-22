f5cda3829a2c300c2f928fade53fb8db8106d240
Ben Thompson <btomo86@gmail.com>
2021-05-22 15:28:49 +0100

    used useContent to get the data for series and films ready to build out the browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { BrowseContainer } from "../containers/browse";
+import { useContext } from "../hooks/";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
-  return <p>Hello from the Browse!</p>;
+  const { series } = useContext("series");
+  const { films } = useContext("films");
+
+  const slides = selectionFilter({ series, films });
+  console.log(slides);
+
+  return <BrowseContainer slides={slides} />;
 }
