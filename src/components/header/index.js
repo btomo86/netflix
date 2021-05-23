78d59a8f3c0e48ac07c6b1598cf3dcb9fd0c5d07
Ben Thompson <btomo86@gmail.com>
2021-05-23 13:54:28 +0100

    added the header button and styled it

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -17,6 +17,7 @@ import {
   Search,
   SearchIcon,
   SearchInput,
+  PlayButton,
 } from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -87,6 +88,10 @@ Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
 
 Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
   return <Link {...restProps}>{children}</Link>;
+};
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+  return <PlayButton {...restProps}>{children}</PlayButton>;
 };
 
 Header.FeatureCallOut = function HeaderFeatureCallOut({
