5098ef350a8d649dc0332a25ee69222faa0aab4b
Ben Thompson <btomo86@gmail.com>
2021-05-23 08:16:49 +0100

    created the profile page with a loading spinner

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { SelectProfileContainer } from "./profiles";
+import { FirebaseContext } from "../context/firebase";
+import { Loading } from "../components";
 
 export function BrowseContainer({ slides }) {
-  return <p>Hello</p>;
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+
+  useEffect(() => {
+    console.log("profile", profile);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+    loading ? (
+      <Loading src={user.photoURL} />
+    ) : null
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
